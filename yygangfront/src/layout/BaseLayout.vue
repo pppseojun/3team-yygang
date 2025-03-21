0d7827b8bfd10130cae068933469fbe281847d59
psj1116 <psjprog1225@gmail.com>
2025-03-21 12:29:43 +0900

    front 초기 개발

---
+++ yygangfront/src/layout/BaseLayout.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="wapper">
+        <Header></Header>
+
+        <RouterView></RouterView>
+
+        <Footer></Footer>
+    </div>
+</template>
+
+<script setup>
+import Footer from '@/components/common/Footer.vue';
+import Header from '@/components/common/Header.vue';
+</script>
+
+<style scoped>
+#wapper{
+  min-height: 100%;
+  position: relative;
+}
+Footer{
+  bottom: 0px;
+  height: 162px;
+  position : absolute;
+}
+</style>
---
+++ yygangfront/src/views/auth/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login">
+        <LoginForm></LoginForm>
+    </div>
+</template>
+
+<script setup>
+import LoginForm from '@/components/form/LoginForm.vue';
+
+
+</script>
+
+<style scoped>
+.login {
+    display: flex;
+    justify-content: center ;
+}
+</style>
